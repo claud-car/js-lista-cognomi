74c71340a857e69a53b15bd9f5c7961abd5e7856
Claudio <claudiocarchietti@yahoo.it>
2021-03-04 17:19:56 +0100

    esercizio terminato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 // LISTA COGNOMI
-var cognomi =["carchietti", "bianchi", "verdi", "rossi", "stone"]
-console.log(cognomi);
+var cognomi =["Carchietti", "Bianchi", "Verdi", "Rossi", "Stone"]
 
 // ORDINO LA LISTA COGNOMI
 cognomi.sort()
@@ -8,6 +7,9 @@ console.log(cognomi);
 
 // CHIEDO COGNOME ALL'UTENTE
 var cognomeutente = prompt("Dimmi il tuo cognome");
+cognomeutente = cognomeutente[0].toUpperCase() + cognomeutente.substring(1);
+console.log(cognomeutente);
+
 // CONTROLLO SE UTENTE HA INSERITO NUMERI
 if (!isNaN(cognomeutente)){
   console.log("Non darmi un numero,dammi una stringa di caratteri");
@@ -19,17 +21,11 @@ if (!isNaN(cognomeutente)){
   console.log(cognomi);
 }
 
-// STAMPO POSIZIONE "UMANA" DELLA LISTA
-// cognomi.slice(1);
-// [1,2,3]
-// console.log(cognomi);
-//
-// cognomi.slice(2, 4);
-// [2, 3]
-//
-// console.log(cognomi);
+// ASSEGNO VARIABILE
+var i= 0;
 
-var cognomi;
-cognomi.splice(0, 0,null)
-
-console.log(cognomi);
+// CICLO WHILE PER STAMPARE LA LISTA DEI COGNOMI
+while (i < cognomi.length) {
+  document.getElementById("cognome").innerHTML += '<li>' + cognomi[i]; + '</li>'
+  ++i;
+}
